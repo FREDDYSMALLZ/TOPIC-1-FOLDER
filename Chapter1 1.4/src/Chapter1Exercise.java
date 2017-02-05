a6560ca82608383804ac2c3994d3bc63ca02f72a
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-05 03:20:33 -0600

    First Commit
    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

---
+++ Chapter1 1.4/src/Chapter1Exercise.java
@@ -0,0 +1,16 @@
+
+public class Chapter1Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Writing a program that displays the table in the console
+		
+			
+	System.out.println("a    a^2    a^3");
+	System.out.println("1    1      1  ");
+	System.out.println("2    4      8  ");
+	System.out.println("3    9      27 ");
+	System.out.println("4   16      64 ");
+	}
+
+}
